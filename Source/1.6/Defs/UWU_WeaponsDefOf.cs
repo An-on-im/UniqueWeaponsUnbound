c9e03ae37810ecbc935ffec9d4b1f18d2e0786f5
An-on-im <urbanist11@gmail.com>
2026-07-24 10:14:03 +0300

    Fixing the ChargeRifle example.

---
+++ Source/1.6/Defs/UWU_WeaponsDefOf.cs
@@ -0,0 +1,11 @@
+using RimWorld;
+using Verse;
+
+namespace UniqueWeaponsUnbound
+{
+    [DefOf]
+    public static class UWU_WeaponsDefOf
+    {
+        public static ResearchProjectDef Gun_ChargeRifle;
+    }
+}
